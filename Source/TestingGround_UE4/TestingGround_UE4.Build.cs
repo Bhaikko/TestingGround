63c90d9b387e5acd1f052b7a22fb8df0bbf9ce64
BHAIKKO\Bhaikko <siddhartha.pawar17@gmail.com>
2018-09-29 13:35:14 +0530

    Behavious Tree Task Node Converted To Blueprint

--- Source/TestingGround_UE4/TestingGround_UE4.Build.cs
+++ Source/TestingGround_UE4/TestingGround_UE4.Build.cs
@@ -8,6 +8,6 @@ public class TestingGround_UE4 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "GameplayTasks" });
 	}
 }
